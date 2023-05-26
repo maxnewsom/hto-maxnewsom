05da83f61d2844f7e4dc6106796b9a55244b1696
maxnewsom <81261968+maxnewsom@users.noreply.github.com>
2023-05-26 07:08:35 -0500

    Update test_cart.py
    repeat test several times to make it needlessly long for demo purposes

--- python/tests/test_cart.py
+++ python/tests/test_cart.py
@@ -60,3 +60,453 @@ def test_remove_from_cart(driver):
 
     items = driver.find_elements(by=By.CSS_SELECTOR, value=".shopping_cart_badge")
     assert 0 == len(items)
+def test_add_from_product(driver):
+    login(driver)
+
+    driver.find_element(by=By.ID, value="item_1_title_link").click()
+    driver.find_element(by=By.ID, value="add-to-cart-sauce-labs-bolt-t-shirt").click()
+
+    items = driver.find_element(by=By.CSS_SELECTOR, value=".shopping_cart_badge")
+    assert "1" == items.text
+
+
+def test_remove_from_product(driver):
+    login(driver)
+
+    driver.find_element(by=By.ID, value="item_1_title_link").click()
+    driver.find_element(by=By.ID, value="add-to-cart-sauce-labs-bolt-t-shirt").click()
+
+    driver.find_element(by=By.ID, value="remove-sauce-labs-bolt-t-shirt").click()
+
+    items = driver.find_elements(by=By.CSS_SELECTOR, value=".shopping_cart_badge")
+    assert 0 == len(items)
+
+
+def test_add_from_inventory(driver):
+    login(driver)
+
+    driver.find_element(by=By.ID, value="add-to-cart-sauce-labs-onesie").click()
+
+    items = driver.find_element(by=By.CSS_SELECTOR, value=".shopping_cart_badge")
+    assert "1" == items.text
+
+
+def test_remove_from_inventory(driver):
+    login(driver)
+    driver.find_element(by=By.ID, value="add-to-cart-sauce-labs-bike-light").click()
+
+    driver.find_element(by=By.ID, value="remove-sauce-labs-bike-light").click()
+
+    items = driver.find_elements(by=By.CSS_SELECTOR, value=".shopping_cart_badge")
+    assert 0 == len(items)
+
+
+def test_remove_from_cart(driver):
+    login(driver)
+    driver.find_element(by=By.ID, value="add-to-cart-sauce-labs-backpack").click()
+    driver.find_element(by=By.CSS_SELECTOR, value=".shopping_cart_link").click()
+
+    driver.find_element(by=By.ID, value="remove-sauce-labs-backpack").click()
+
+    items = driver.find_elements(by=By.CSS_SELECTOR, value=".shopping_cart_badge")
+    assert 0 == len(items)
+def test_add_from_product(driver):
+    login(driver)
+
+    driver.find_element(by=By.ID, value="item_1_title_link").click()
+    driver.find_element(by=By.ID, value="add-to-cart-sauce-labs-bolt-t-shirt").click()
+
+    items = driver.find_element(by=By.CSS_SELECTOR, value=".shopping_cart_badge")
+    assert "1" == items.text
+
+
+def test_remove_from_product(driver):
+    login(driver)
+
+    driver.find_element(by=By.ID, value="item_1_title_link").click()
+    driver.find_element(by=By.ID, value="add-to-cart-sauce-labs-bolt-t-shirt").click()
+
+    driver.find_element(by=By.ID, value="remove-sauce-labs-bolt-t-shirt").click()
+
+    items = driver.find_elements(by=By.CSS_SELECTOR, value=".shopping_cart_badge")
+    assert 0 == len(items)
+
+
+def test_add_from_inventory(driver):
+    login(driver)
+
+    driver.find_element(by=By.ID, value="add-to-cart-sauce-labs-onesie").click()
+
+    items = driver.find_element(by=By.CSS_SELECTOR, value=".shopping_cart_badge")
+    assert "1" == items.text
+
+
+def test_remove_from_inventory(driver):
+    login(driver)
+    driver.find_element(by=By.ID, value="add-to-cart-sauce-labs-bike-light").click()
+
+    driver.find_element(by=By.ID, value="remove-sauce-labs-bike-light").click()
+
+    items = driver.find_elements(by=By.CSS_SELECTOR, value=".shopping_cart_badge")
+    assert 0 == len(items)
+
+
+def test_remove_from_cart(driver):
+    login(driver)
+    driver.find_element(by=By.ID, value="add-to-cart-sauce-labs-backpack").click()
+    driver.find_element(by=By.CSS_SELECTOR, value=".shopping_cart_link").click()
+
+    driver.find_element(by=By.ID, value="remove-sauce-labs-backpack").click()
+
+    items = driver.find_elements(by=By.CSS_SELECTOR, value=".shopping_cart_badge")
+    assert 0 == len(items)
+def test_add_from_product(driver):
+    login(driver)
+
+    driver.find_element(by=By.ID, value="item_1_title_link").click()
+    driver.find_element(by=By.ID, value="add-to-cart-sauce-labs-bolt-t-shirt").click()
+
+    items = driver.find_element(by=By.CSS_SELECTOR, value=".shopping_cart_badge")
+    assert "1" == items.text
+
+
+def test_remove_from_product(driver):
+    login(driver)
+
+    driver.find_element(by=By.ID, value="item_1_title_link").click()
+    driver.find_element(by=By.ID, value="add-to-cart-sauce-labs-bolt-t-shirt").click()
+
+    driver.find_element(by=By.ID, value="remove-sauce-labs-bolt-t-shirt").click()
+
+    items = driver.find_elements(by=By.CSS_SELECTOR, value=".shopping_cart_badge")
+    assert 0 == len(items)
+
+
+def test_add_from_inventory(driver):
+    login(driver)
+
+    driver.find_element(by=By.ID, value="add-to-cart-sauce-labs-onesie").click()
+
+    items = driver.find_element(by=By.CSS_SELECTOR, value=".shopping_cart_badge")
+    assert "1" == items.text
+
+
+def test_remove_from_inventory(driver):
+    login(driver)
+    driver.find_element(by=By.ID, value="add-to-cart-sauce-labs-bike-light").click()
+
+    driver.find_element(by=By.ID, value="remove-sauce-labs-bike-light").click()
+
+    items = driver.find_elements(by=By.CSS_SELECTOR, value=".shopping_cart_badge")
+    assert 0 == len(items)
+
+
+def test_remove_from_cart(driver):
+    login(driver)
+    driver.find_element(by=By.ID, value="add-to-cart-sauce-labs-backpack").click()
+    driver.find_element(by=By.CSS_SELECTOR, value=".shopping_cart_link").click()
+
+    driver.find_element(by=By.ID, value="remove-sauce-labs-backpack").click()
+
+    items = driver.find_elements(by=By.CSS_SELECTOR, value=".shopping_cart_badge")
+    assert 0 == len(items)
+def test_add_from_product(driver):
+    login(driver)
+
+    driver.find_element(by=By.ID, value="item_1_title_link").click()
+    driver.find_element(by=By.ID, value="add-to-cart-sauce-labs-bolt-t-shirt").click()
+
+    items = driver.find_element(by=By.CSS_SELECTOR, value=".shopping_cart_badge")
+    assert "1" == items.text
+
+
+def test_remove_from_product(driver):
+    login(driver)
+
+    driver.find_element(by=By.ID, value="item_1_title_link").click()
+    driver.find_element(by=By.ID, value="add-to-cart-sauce-labs-bolt-t-shirt").click()
+
+    driver.find_element(by=By.ID, value="remove-sauce-labs-bolt-t-shirt").click()
+
+    items = driver.find_elements(by=By.CSS_SELECTOR, value=".shopping_cart_badge")
+    assert 0 == len(items)
+
+
+def test_add_from_inventory(driver):
+    login(driver)
+
+    driver.find_element(by=By.ID, value="add-to-cart-sauce-labs-onesie").click()
+
+    items = driver.find_element(by=By.CSS_SELECTOR, value=".shopping_cart_badge")
+    assert "1" == items.text
+
+
+def test_remove_from_inventory(driver):
+    login(driver)
+    driver.find_element(by=By.ID, value="add-to-cart-sauce-labs-bike-light").click()
+
+    driver.find_element(by=By.ID, value="remove-sauce-labs-bike-light").click()
+
+    items = driver.find_elements(by=By.CSS_SELECTOR, value=".shopping_cart_badge")
+    assert 0 == len(items)
+
+
+def test_remove_from_cart(driver):
+    login(driver)
+    driver.find_element(by=By.ID, value="add-to-cart-sauce-labs-backpack").click()
+    driver.find_element(by=By.CSS_SELECTOR, value=".shopping_cart_link").click()
+
+    driver.find_element(by=By.ID, value="remove-sauce-labs-backpack").click()
+
+    items = driver.find_elements(by=By.CSS_SELECTOR, value=".shopping_cart_badge")
+    assert 0 == len(items)
+def test_add_from_product(driver):
+    login(driver)
+
+    driver.find_element(by=By.ID, value="item_1_title_link").click()
+    driver.find_element(by=By.ID, value="add-to-cart-sauce-labs-bolt-t-shirt").click()
+
+    items = driver.find_element(by=By.CSS_SELECTOR, value=".shopping_cart_badge")
+    assert "1" == items.text
+
+
+def test_remove_from_product(driver):
+    login(driver)
+
+    driver.find_element(by=By.ID, value="item_1_title_link").click()
+    driver.find_element(by=By.ID, value="add-to-cart-sauce-labs-bolt-t-shirt").click()
+
+    driver.find_element(by=By.ID, value="remove-sauce-labs-bolt-t-shirt").click()
+
+    items = driver.find_elements(by=By.CSS_SELECTOR, value=".shopping_cart_badge")
+    assert 0 == len(items)
+
+
+def test_add_from_inventory(driver):
+    login(driver)
+
+    driver.find_element(by=By.ID, value="add-to-cart-sauce-labs-onesie").click()
+
+    items = driver.find_element(by=By.CSS_SELECTOR, value=".shopping_cart_badge")
+    assert "1" == items.text
+
+
+def test_remove_from_inventory(driver):
+    login(driver)
+    driver.find_element(by=By.ID, value="add-to-cart-sauce-labs-bike-light").click()
+
+    driver.find_element(by=By.ID, value="remove-sauce-labs-bike-light").click()
+
+    items = driver.find_elements(by=By.CSS_SELECTOR, value=".shopping_cart_badge")
+    assert 0 == len(items)
+
+
+def test_remove_from_cart(driver):
+    login(driver)
+    driver.find_element(by=By.ID, value="add-to-cart-sauce-labs-backpack").click()
+    driver.find_element(by=By.CSS_SELECTOR, value=".shopping_cart_link").click()
+
+    driver.find_element(by=By.ID, value="remove-sauce-labs-backpack").click()
+
+    items = driver.find_elements(by=By.CSS_SELECTOR, value=".shopping_cart_badge")
+    assert 0 == len(items)
+def test_add_from_product(driver):
+    login(driver)
+
+    driver.find_element(by=By.ID, value="item_1_title_link").click()
+    driver.find_element(by=By.ID, value="add-to-cart-sauce-labs-bolt-t-shirt").click()
+
+    items = driver.find_element(by=By.CSS_SELECTOR, value=".shopping_cart_badge")
+    assert "1" == items.text
+
+
+def test_remove_from_product(driver):
+    login(driver)
+
+    driver.find_element(by=By.ID, value="item_1_title_link").click()
+    driver.find_element(by=By.ID, value="add-to-cart-sauce-labs-bolt-t-shirt").click()
+
+    driver.find_element(by=By.ID, value="remove-sauce-labs-bolt-t-shirt").click()
+
+    items = driver.find_elements(by=By.CSS_SELECTOR, value=".shopping_cart_badge")
+    assert 0 == len(items)
+
+
+def test_add_from_inventory(driver):
+    login(driver)
+
+    driver.find_element(by=By.ID, value="add-to-cart-sauce-labs-onesie").click()
+
+    items = driver.find_element(by=By.CSS_SELECTOR, value=".shopping_cart_badge")
+    assert "1" == items.text
+
+
+def test_remove_from_inventory(driver):
+    login(driver)
+    driver.find_element(by=By.ID, value="add-to-cart-sauce-labs-bike-light").click()
+
+    driver.find_element(by=By.ID, value="remove-sauce-labs-bike-light").click()
+
+    items = driver.find_elements(by=By.CSS_SELECTOR, value=".shopping_cart_badge")
+    assert 0 == len(items)
+
+
+def test_remove_from_cart(driver):
+    login(driver)
+    driver.find_element(by=By.ID, value="add-to-cart-sauce-labs-backpack").click()
+    driver.find_element(by=By.CSS_SELECTOR, value=".shopping_cart_link").click()
+
+    driver.find_element(by=By.ID, value="remove-sauce-labs-backpack").click()
+
+    items = driver.find_elements(by=By.CSS_SELECTOR, value=".shopping_cart_badge")
+    assert 0 == len(items)
+def test_add_from_product(driver):
+    login(driver)
+
+    driver.find_element(by=By.ID, value="item_1_title_link").click()
+    driver.find_element(by=By.ID, value="add-to-cart-sauce-labs-bolt-t-shirt").click()
+
+    items = driver.find_element(by=By.CSS_SELECTOR, value=".shopping_cart_badge")
+    assert "1" == items.text
+
+
+def test_remove_from_product(driver):
+    login(driver)
+
+    driver.find_element(by=By.ID, value="item_1_title_link").click()
+    driver.find_element(by=By.ID, value="add-to-cart-sauce-labs-bolt-t-shirt").click()
+
+    driver.find_element(by=By.ID, value="remove-sauce-labs-bolt-t-shirt").click()
+
+    items = driver.find_elements(by=By.CSS_SELECTOR, value=".shopping_cart_badge")
+    assert 0 == len(items)
+
+
+def test_add_from_inventory(driver):
+    login(driver)
+
+    driver.find_element(by=By.ID, value="add-to-cart-sauce-labs-onesie").click()
+
+    items = driver.find_element(by=By.CSS_SELECTOR, value=".shopping_cart_badge")
+    assert "1" == items.text
+
+
+def test_remove_from_inventory(driver):
+    login(driver)
+    driver.find_element(by=By.ID, value="add-to-cart-sauce-labs-bike-light").click()
+
+    driver.find_element(by=By.ID, value="remove-sauce-labs-bike-light").click()
+
+    items = driver.find_elements(by=By.CSS_SELECTOR, value=".shopping_cart_badge")
+    assert 0 == len(items)
+
+
+def test_remove_from_cart(driver):
+    login(driver)
+    driver.find_element(by=By.ID, value="add-to-cart-sauce-labs-backpack").click()
+    driver.find_element(by=By.CSS_SELECTOR, value=".shopping_cart_link").click()
+
+    driver.find_element(by=By.ID, value="remove-sauce-labs-backpack").click()
+
+    items = driver.find_elements(by=By.CSS_SELECTOR, value=".shopping_cart_badge")
+    assert 0 == len(items)
+def test_add_from_product(driver):
+    login(driver)
+
+    driver.find_element(by=By.ID, value="item_1_title_link").click()
+    driver.find_element(by=By.ID, value="add-to-cart-sauce-labs-bolt-t-shirt").click()
+
+    items = driver.find_element(by=By.CSS_SELECTOR, value=".shopping_cart_badge")
+    assert "1" == items.text
+
+
+def test_remove_from_product(driver):
+    login(driver)
+
+    driver.find_element(by=By.ID, value="item_1_title_link").click()
+    driver.find_element(by=By.ID, value="add-to-cart-sauce-labs-bolt-t-shirt").click()
+
+    driver.find_element(by=By.ID, value="remove-sauce-labs-bolt-t-shirt").click()
+
+    items = driver.find_elements(by=By.CSS_SELECTOR, value=".shopping_cart_badge")
+    assert 0 == len(items)
+
+
+def test_add_from_inventory(driver):
+    login(driver)
+
+    driver.find_element(by=By.ID, value="add-to-cart-sauce-labs-onesie").click()
+
+    items = driver.find_element(by=By.CSS_SELECTOR, value=".shopping_cart_badge")
+    assert "1" == items.text
+
+
+def test_remove_from_inventory(driver):
+    login(driver)
+    driver.find_element(by=By.ID, value="add-to-cart-sauce-labs-bike-light").click()
+
+    driver.find_element(by=By.ID, value="remove-sauce-labs-bike-light").click()
+
+    items = driver.find_elements(by=By.CSS_SELECTOR, value=".shopping_cart_badge")
+    assert 0 == len(items)
+
+
+def test_remove_from_cart(driver):
+    login(driver)
+    driver.find_element(by=By.ID, value="add-to-cart-sauce-labs-backpack").click()
+    driver.find_element(by=By.CSS_SELECTOR, value=".shopping_cart_link").click()
+
+    driver.find_element(by=By.ID, value="remove-sauce-labs-backpack").click()
+
+    items = driver.find_elements(by=By.CSS_SELECTOR, value=".shopping_cart_badge")
+    assert 0 == len(items)
+def test_add_from_product(driver):
+    login(driver)
+
+    driver.find_element(by=By.ID, value="item_1_title_link").click()
+    driver.find_element(by=By.ID, value="add-to-cart-sauce-labs-bolt-t-shirt").click()
+
+    items = driver.find_element(by=By.CSS_SELECTOR, value=".shopping_cart_badge")
+    assert "1" == items.text
+
+
+def test_remove_from_product(driver):
+    login(driver)
+
+    driver.find_element(by=By.ID, value="item_1_title_link").click()
+    driver.find_element(by=By.ID, value="add-to-cart-sauce-labs-bolt-t-shirt").click()
+
+    driver.find_element(by=By.ID, value="remove-sauce-labs-bolt-t-shirt").click()
+
+    items = driver.find_elements(by=By.CSS_SELECTOR, value=".shopping_cart_badge")
+    assert 0 == len(items)
+
+
+def test_add_from_inventory(driver):
+    login(driver)
+
+    driver.find_element(by=By.ID, value="add-to-cart-sauce-labs-onesie").click()
+
+    items = driver.find_element(by=By.CSS_SELECTOR, value=".shopping_cart_badge")
+    assert "1" == items.text
+
+
+def test_remove_from_inventory(driver):
+    login(driver)
+    driver.find_element(by=By.ID, value="add-to-cart-sauce-labs-bike-light").click()
+
+    driver.find_element(by=By.ID, value="remove-sauce-labs-bike-light").click()
+
+    items = driver.find_elements(by=By.CSS_SELECTOR, value=".shopping_cart_badge")
+    assert 0 == len(items)
+
+
+def test_remove_from_cart(driver):
+    login(driver)
+    driver.find_element(by=By.ID, value="add-to-cart-sauce-labs-backpack").click()
+    driver.find_element(by=By.CSS_SELECTOR, value=".shopping_cart_link").click()
+
+    driver.find_element(by=By.ID, value="remove-sauce-labs-backpack").click()
+
+    items = driver.find_elements(by=By.CSS_SELECTOR, value=".shopping_cart_badge")
+    assert 0 == len(items)                            
