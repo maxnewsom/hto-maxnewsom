74cdeb6be58efe817b1a8b54676a3e0965fe02e6
maxnewsom <81261968+maxnewsom@users.noreply.github.com>
2023-06-20 16:17:15 -0500

    Update AuthenticationTest.java
    fixed loginWorks to make more consistent

--- java/src/test/java/com/saucelabs/hto/AuthenticationTest.java
+++ java/src/test/java/com/saucelabs/hto/AuthenticationTest.java
@@ -44,17 +44,11 @@ public class AuthenticationTest extends BaseTest {
   }
     @Test
     public void loginWorks() throws InterruptedException {
-    for(int i=0; i<105; i++) {
+    for(int i=0; i<205; i++) {
     driver.get("https://www.saucedemo.com/");
     driver.findElement(By.id("user-name")).sendKeys("standard_user");
     driver.findElement(By.id("password")).sendKeys("secret_sauce");
     driver.findElement(By.id("login-button")).click();
-    driver.findElement(By.id("react-burger-menu-btn")).click();
-    Thread.sleep(1000);
-
-    driver.findElement(By.id("logout_sidebar_link")).click();
-
-    Assertions.assertEquals("https://www.saucedemo.com/", driver.getCurrentUrl());
     }
 }
 }
